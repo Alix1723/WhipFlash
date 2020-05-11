980713e196c45aa272d8c722d46a7dd1c864c0df
Alix <alix1723@live.co.uk>
2020-05-11 15:42:56 +0100

    Bugfix Python script

--- LightFxServer/LightFxServer/PythonClient/midi-input-client.py
+++ LightFxServer/LightFxServer/PythonClient/midi-input-client.py
@@ -6,7 +6,7 @@ inputSelection = -1
 
 TCP_IP = '127.0.0.1'
 TCP_PORT = 5005
-BUFFER_SIZE = 512
+BUFFER_SIZE = 256
 
 if(len(sys.argv) > 1):
 	inputSelection = int(sys.argv[1])
@@ -19,19 +19,19 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
 print("Successfully connected to ", TCP_IP, TCP_PORT) 
 
-if(inputSelection = -1):
+if(inputSelection == -1):
 	for x in range(0, pygame.midi.get_count()):
 	    print("Device", x,pygame.midi.get_device_info(x))
  
 	inputSelection = int(input())
 
 inp = pygame.midi.Input(inputSelection) #Todo: configurable
-print("reading device #, #",chosenvalue,pygame.midi.get_device_info(chosenvalue))
+print("reading device #, #",inputSelection,pygame.midi.get_device_info(inputSelection))
 
 while True:
     
     if inp.poll():
-        midi_events = inp.read(256)
+        midi_events = inp.read(BUFFER_SIZE)
         for m_e in midi_events:
             print("event")
             message = "{},{},{},{},{},".format(m_e[0][0], m_e[0][1], m_e[0][2], m_e[0][3], m_e[1])
